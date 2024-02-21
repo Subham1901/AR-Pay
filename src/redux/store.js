3c9a190bb608df31c2e83e9f3e7c16c0fc39b141
Subham Prasad Panda <79370554+Subham1901@users.noreply.github.com>
2024-02-22 00:14:27 +0530

    reduc store fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { createStore } from "redux";
 import { rootReducer } from "./rootReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(rootReducer);
